16150350c18b8d5443c77290a1e495dfc8aa3ede
danymarsico <danymarsico@gmail.com>
2023-11-05 00:36:17 -0400

    Submits solutions to 2 additional exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(array, ...checks) {
+const finalArray = [];
+array.forEach(element => {
+    if(!checks.includes(element)) {
+        finalArray.push(element);
+        }
+    });
+        return finalArray;
+    };
 
 // Do not edit below this line
 module.exports = removeFromArray;
